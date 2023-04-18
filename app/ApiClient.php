4ce4ba734ab7dad66dcda8f254ae040a9470c4ce
Guntars <gvaivods@gmail.com>
2023-04-18 16:36:48 +0300

    update

--- app/ApiClient.php
+++ app/ApiClient.php
@@ -14,12 +14,14 @@ class ApiClient
         $this->client = new Client();
     }
 
-    public function getReport(int $regCode): BusinessReport
+    public function getReport(int $regCode): ?BusinessReport
     {
         $url = "https://data.gov.lv/dati/lv/api/3/action/datastore_search?q={$regCode}&resource_id=25e80bf3-f107-4ab4-89ef-251b5b9374e9";
         $response = $this->client->request('GET', $url);
         $businessData = json_decode($response->getBody()->getContents());
-
+        if (count($businessData->result->records) === 0) {
+            return null;
+        }
         return new BusinessReport(
             $businessData->result->records[0]->name,
             $businessData->result->records[0]->sepa,
--- main.php
+++ main.php
@@ -4,14 +4,24 @@ use App\ApiClient;
 
 require_once "vendor/autoload.php";
 
-$regCode = (int)readline("Please enter registration number: ");
-$apiClient = new ApiClient();
-$businessReport = $apiClient->getReport($regCode);
+$regCode = readline("Please enter registration number: ");
 
-echo "--------------------------------------------------------------------" . PHP_EOL;
-echo "Business report for registration number: $regCode" . PHP_EOL;
-echo "Business title: " . $businessReport->getName() . PHP_EOL;
-echo "SEPA identification: " . $businessReport->getSepa() . PHP_EOL;
-echo "Address: " . $businessReport->getAddress() . PHP_EOL;
-echo "Registration date: " . $businessReport->getRegistred() . PHP_EOL;
-echo "Rank: " . $businessReport->getRank() . PHP_EOL;
+if (!is_numeric($regCode)) {
+    echo "Error: Input must be a number" . PHP_EOL;
+    exit;
+}
+
+$apiClient = new ApiClient();
+$businessReport = $apiClient->getReport((int)$regCode);
+
+if ($businessReport != null) {
+    echo "--------------------------------------------------------------------" . PHP_EOL;
+    echo "Business report for registration number: $regCode" . PHP_EOL;
+    echo "Business title: " . $businessReport->getName() . PHP_EOL;
+    echo "SEPA identification: " . $businessReport->getSepa() . PHP_EOL;
+    echo "Address: " . $businessReport->getAddress() . PHP_EOL;
+    echo "Registration date: " . $businessReport->getRegistred() . PHP_EOL;
+    echo "Rank: " . $businessReport->getRank() . PHP_EOL;
+} else {
+    echo "There is no such company. Please try again" . PHP_EOL;
+}
